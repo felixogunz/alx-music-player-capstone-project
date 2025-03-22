438cf462a3a9e22b5ff7c75b0519d5cae7346152
Felix Ogunbowale <167327564+felixogunz@users.noreply.github.com>
2025-03-22 20:34:40 +0100

    Created Sidebar Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
+    /*<div className='h-screen bg-black'>*/
+    <div className="w-screen h-screen bg-black flex items-center justify-center">
       <div className='h-[90%] flex'>
         <Sidebar />
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,39 +1,88 @@
-import React from 'react'
-import {assets} from '../assets/assets'
+import React from 'react';
+import { assets } from '../assets/assets';
 
-const Sidebar= () => {
+const Sidebar = () => {
   return (
-    <div className='w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex'>
-        <div className='bg-[#121212] h-[15%] rounded flex flex-col justify-around'>
-            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                <img className='w-6' src={assets.home_icon} alt="" />
-                <p className='font-bold'>Home</p>
-            </div>
-            <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-                <img className='w-6' src={assets.search_icon} alt="" />
-                <p className='font-bold'>Search</p>
-            </div>
+    <div className='w-[20%] h-screen p-4 flex flex-col gap-4 text-white bg-[#121212] fixed top-0 left-0 lg:flex'>
+      {/* Home & Search Section */}
+      <div className='rounded flex flex-col gap-4'>
+        <div className='flex items-center gap-3 p-3 cursor-pointer hover:bg-[#242424] rounded'>
+          <img className='w-6' src={assets.home_icon} alt='Home' />
+          <p className='font-bold text-sm'>Home</p>
         </div>
-        <div className='bg-[#121212] h-[85%] rounded'>
-            <div className='p-4 flex items-center justify-between'>
-                <div className='flex items-center gap-3'>
-                    <img className='w-8' src={assets.stack_icon} alt="" />
-                    <p className='font-semibold'>Your Library</p>
-                </div>
-                <div className='flex items-center gap-3'>
-                    <img className='w-5' src={assets.arrow_icon} alt="" />
-                    <img className='w-5' src={assets.plus_icon} alt="" />
-                </div>
-            </div>
-            <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4'>
-                <h1>Create your first playlist</h1>
-                <p className='font-light'>It's easy, we'll help you</p>
-                <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Create Playlist</button>
-
-            </div>
+        <div className='flex items-center gap-3 p-3 cursor-pointer hover:bg-[#242424] rounded'>
+          <img className='w-6' src={assets.search_icon} alt='Search' />
+          <p className='font-bold text-sm'>Search</p>
         </div>
+      </div>
+      
+      {/* Your Library Section */}
+      <div className='flex flex-col flex-grow bg-[#121212] rounded'>
+        <div className='p-3 flex items-center justify-between'>
+          <div className='flex items-center gap-3'>
+            <img className='w-6' src={assets.stack_icon} alt='Library' />
+            <p className='font-semibold text-sm'>Your Library</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <img className='w-4 cursor-pointer' src={assets.plus_icon} alt='Add'/>
+            <img className='w-4 cursor-pointer' src={assets.arrow_icon} alt='More'/>
+          </div>
+        </div>
+        
+        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start gap-2'>
+          <h1 className='text-sm'>Create your first playlist</h1>
+          <p className='text-xs font-light'>It's easy, we'll help you</p>
+          <button className='px-4 py-1 bg-white text-black text-xs rounded-full mt-2'>Create Playlist</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar 
+export default Sidebar;
+
+
+/*
+import React from 'react';
+import { assets } from '../assets/assets';
+
+const Sidebar = () => {
+  return (
+    <div className='w-[12%] h-screen p-4 flex flex-col gap-4 text-white bg-[#121212] fixed top-0 left-0 lg:flex'>
+      {/* Home & Search Section }
+      <div className='rounded flex flex-col gap-4'>
+        <div className='flex items-center gap-3 p-3 cursor-pointer hover:bg-[#242424] rounded'>
+          <img className='w-6' src={assets.home_icon} alt='Home' />
+          <p className='font-bold text-sm'>Home</p>
+        </div>
+        <div className='flex items-center gap-3 p-3 cursor-pointer hover:bg-[#242424] rounded'>
+          <img className='w-6' src={assets.search_icon} alt='Search' />
+          <p className='font-bold text-sm'>Search</p>
+        </div>
+      </div>
+      
+      {/* Your Library Section }
+      <div className='flex flex-col flex-grow bg-[#121212] rounded'>
+        <div className='p-3 flex items-center justify-between'>
+          <div className='flex items-center gap-3'>
+            <img className='w-6' src={assets.stack_icon} alt='Library' />
+            <p className='font-semibold text-sm'>Your Library</p>
+          </div>
+          <div className='flex items-center gap-2'>
+            <img className='w-4 cursor-pointer' src={assets.plus_icon} alt='Add' />
+            <img className='w-4 cursor-pointer' src={assets.arrow_icon} alt='More'/>
+          </div>
+        </div>
+        
+        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start gap-2'>
+          <h1 className='text-sm'>Create your first playlist</h1>
+          <p className='text-xs font-light'>It's easy, we'll help you</p>
+          <button className='px-4 py-1 bg-white text-black text-xs rounded-full mt-2'>Create Playlist</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
+*/
